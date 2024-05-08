085295dcf176a4d9b36b18b9a4de2855a1812f14
SiddharthPatel-10 <connectwithsiddh@gmail.com>
2024-05-09 00:20:20 +0530

    largest element in an Array

---
+++ index.js
@@ -0,0 +1,5 @@
+// Write a JavaScript function to find the largest element in an array of integers without using any built-in methods like Math.max() or sorting algorithms. Optimize your solution for time complexity.
+
+const array = [3, 7, 12, 8, 5];
+const largest = Math.max(...array);
+console.log(largest);
