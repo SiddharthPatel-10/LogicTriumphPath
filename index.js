5d515b5f470f38d13b16af12bd40dd243dd0b498
SiddharthPatel-10 <connectwithsiddh@gmail.com>
2024-05-10 23:37:25 +0530

    question added

--- index.js
+++ index.js
@@ -3,3 +3,7 @@
 const array = [3, 7, 12, 8, 5];
 const largest = Math.max(...array);
 console.log(largest);
+
+
+//Given an array Arr of size N, print the second largest distinct element from an array. If the second largest element doesn't exist then return -1.
+
