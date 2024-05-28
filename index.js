5f3e3d5b907c83d185e85d9529adc5c503312393
SiddharthPatel-10 <connectwithsiddh@gmail.com>
2024-05-29 01:37:27 +0530

    Linear search on an Array

--- index.js
+++ index.js
@@ -21,27 +21,23 @@
 //   return true;
 // }
 
-
 // console.log(isArraySorted(sortedArray));
 // console.log(isArraySorted(unsortedArray));
-
-
 
 // Q.3 You are given an array of integers. Left rotate the array by one place.
 
 // function leftRotateByOne(nums) {
-//   if (nums.length <= 1) return nums; 
+//   if (nums.length <= 1) return nums;
 //   const firstElement = nums.shift();
-//   nums.push(firstElement); 
+//   nums.push(firstElement);
 //   return nums;
 // }
 
 // const nums1 = [1, 2, 3, 4, 5];
-// console.log(leftRotateByOne(nums1)); 
+// console.log(leftRotateByOne(nums1));
 
 // const nums2 = [9, 8, 7, 6, 5, 4, 3, 2, 1];
-// console.log(leftRotateByOne(nums2)); 
-
+// console.log(leftRotateByOne(nums2));
 
 // Q.4  You are given an array of integers. Left rotate the array by D place.
 
@@ -66,38 +62,54 @@
 // const arr = [1, 2, 3, 4, 5, 6, 7, 8];
 // const D = 2;
 // leftRotateArray(arr, D);
-// console.log(arr); 
+// console.log(arr);
 // time complexity : O(n)
 // space complexity : O(1)
 
-
 // Q.5 move all the zero's at the end of the Array
-function moveZerosToEnd(arr) {
-  let nonZeroElements = [];
-  let zeroCount = 0;
+// function moveZerosToEnd(arr) {
+//   let nonZeroElements = [];
+//   let zeroCount = 0;
 
-  // Iterate through the array and segregate non-zero elements and count zeros
-  for (let i = 0; i < arr.length; i++) {
-      if (arr[i] !== 0) {
-          nonZeroElements.push(arr[i]);
-      } else {
-          zeroCount++;
-      }
-  }
+//   // Iterate through the array and segregate non-zero elements and count zeros
+//   for (let i = 0; i < arr.length; i++) {
+//       if (arr[i] !== 0) {
+//           nonZeroElements.push(arr[i]);
+//       } else {
+//           zeroCount++;
+//       }
+//   }
 
-  // Add the counted zeros to the end of the non-zero elements array
-  while (zeroCount > 0) {
-      nonZeroElements.push(0);
-      zeroCount--;
-  }
+//   // Add the counted zeros to the end of the non-zero elements array
+//   while (zeroCount > 0) {
+//       nonZeroElements.push(0);
+//       zeroCount--;
+//   }
 
-  return nonZeroElements;
-}
+//   return nonZeroElements;
+// }
 
-// Example usage:
-let arr = [0, 1, 0, 3, 12];
-let result = moveZerosToEnd(arr);
-console.log(result); // Output: [1, 3, 12, 0, 0]
+// // Example usage:
+// let arr = [0, 1, 0, 3, 12];
+// let result = moveZerosToEnd(arr);
+// console.log(result); // Output: [1, 3, 12, 0, 0]
 
 //time complexity: O(n)
 //space complexity: O(n)
+
+//Q.6 Linear search on an Array
+
+function linearSearch(arr, n) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == n) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 8];
+n = 6;
+console.log(linearSearch(arr, n));  //Output: 5
+//time complexity: O(n)
+//space complexity: O(1)
